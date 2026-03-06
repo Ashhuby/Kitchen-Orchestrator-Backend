62d2714c2a76372ede99b616e1012325710b4856
Marwan Ashhuby <mashhuby@gmail.com>
2026-03-06 22:44:00 +0000

    chore: remove duplicate configuration classes and fix namespace collisions

--- src/IdentityAPI/Configuration/ServerAuthOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace KitchenOrchestrator.IdentityAPI.Configuration
-{
-    public class ServerAuthOptions
-    {
-        public string SharedSecret { get; set; } = string.Empty; // Used by HmacHelper
-    }
-}
