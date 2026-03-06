febe12829aca80177a550abcb2add52aa1e50c3f
Marwan Ashhuby <mashhuby@gmail.com>
2026-03-06 18:52:57 +0000

    MatchSession

---
+++ src/GameServer/Models/MatchSession.cs
@@ -0,0 +1,33 @@
+using KitchenOrchestrator.Shared.Contracts.Enums;
+using KitchenOrchestrator.Shared.GameLogic.Levels;
+
+namespace KitchenOrchestrator.GameServer.Models
+{
+    public class MatchSession
+    {
+        public Guid SessionId { get; }
+        public string LevelId { get; }
+        public MatchState State { get; set; }
+        public DateTime StartedAtUtc { get; private set; }
+        public float TimeRemainingSeconds { get; set; }
+        public int TotalScore { get; set; }
+        public List<ConnectedPlayer> Players { get; }
+        public List<ActiveOrder> Orders { get; }
+
+        public MatchSession(LevelDefinition level)
+        {
+            SessionId = Guid.NewGuid();
+            LevelId = level.LevelId; // Pulled directly from the definition
+            State = MatchState.Lobby;
+            TimeRemainingSeconds = level.DurationSeconds;
+            Players = new List<ConnectedPlayer>();
+            Orders = new List<ActiveOrder>();
+        }
+
+        public void Start()
+        {
+            State = MatchState.Active;
+            StartedAtUtc = DateTime.UtcNow;
+        }
+    }
+}
